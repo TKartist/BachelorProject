30cdaabdcc5087f106b709bdcf98aa1f83cb0b8e
TKartist <rimh@usi.ch>
2024-02-26 16:57:56 +0100

    different visualization options offered

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -5,15 +5,31 @@ import matplotlib.pyplot as plt
 
 def visualize_country(country_name):
     df = dr.get_country_data(country_name)
-    for item in df["item"].unique():
-        item_data = df[df["item"] == item]
-        plt.scatter(item_data.index, item_data["value"], label=item)
-    plt.title("Rebasement Values by Item")
+    energy_array = df["item"].unique()
+    print(energy_array)
+    item_type = int(
+        input(
+            "Which of the energy sources would you like to visualize, \n please enter 0 if all, otherwise input index of the item (from 1): "
+        )
+    )
+
+    if item_type == 0:
+        for item in df["item"].unique():
+            item_data = df[df["item"] == item]
+            plt.scatter(item_data.index, item_data["value"], label=item)
+    elif item_type <= len(energy_array):
+        item_data = df[df["item"] == energy_array[item_type - 1]]
+        plt.scatter(
+            item_data.index, item_data["value"], label=energy_array[item_type - 1]
+        )
+    else:
+        return
+    plt.title("Values by Item")
     plt.xlabel("Index")
-    plt.ylabel("Rebasement")
+    plt.ylabel("Values")
     plt.legend()
     plt.grid(True, linestyle="--", alpha=0.7)
     plt.show()
 
 
-visualize_country("Germany")
+visualize_country("Switzerland")
