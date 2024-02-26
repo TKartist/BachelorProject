48a5aab9ff13751e601ed7b39e697d175af29f5f
TKartist <rimh@usi.ch>
2024-02-26 17:33:51 +0100

    showing both scatter and line plot together for better visualization

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 def visualize_country(country_name):
     df = dr.get_country_data(country_name)
     energy_array = df["item"].unique()
+
     print(energy_array)
     item_type = int(
         input(
@@ -13,22 +14,40 @@ def visualize_country(country_name):
         )
     )
 
+    fig, axs = plt.subplots(2, 1, figsize=(8, 10))
     if item_type == 0:
         for item in df["item"].unique():
             item_data = df[df["item"] == item]
-            plt.scatter(item_data.index, item_data["value"], label=item)
+            axs[0].plot(item_data.index, item_data["value"], marker="o", label=item)
+            axs[1].scatter(item_data.index, item_data["value"], marker="o", label=item)
     elif item_type <= len(energy_array):
         item_data = df[df["item"] == energy_array[item_type - 1]]
-        plt.scatter(
-            item_data.index, item_data["value"], label=energy_array[item_type - 1]
+        axs[0].plot(
+            item_data.index,
+            item_data["value"],
+            marker="o",
+            label=energy_array[item_type - 1],
+        )
+        axs[1].scatter(
+            item_data.index,
+            item_data["value"],
+            marker="o",
+            label=energy_array[item_type - 1],
         )
     else:
         return
-    plt.title("Values by Item")
-    plt.xlabel("Index")
-    plt.ylabel("Values")
-    plt.legend()
-    plt.grid(True, linestyle="--", alpha=0.7)
+
+    axs[0].set_title("Values by Item Line Plot")
+    axs[0].set_xlabel("Index")
+    axs[0].set_ylabel("Values")
+    axs[0].legend()
+    axs[0].grid(True, linestyle="--", alpha=0.7)
+    axs[1].set_title("Values by Item Scatter Plot")
+    axs[1].set_xlabel("Index")
+    axs[1].set_ylabel("Values")
+    axs[1].legend()
+    axs[1].grid(True, linestyle="--", alpha=0.7)
+
     plt.show()
 
 
