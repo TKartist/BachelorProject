b7693aa52a0228e34e7858188d70d0362c2eb972
TKartist <rimh@usi.ch>
2024-02-26 13:55:53 +0100

    data reading done

--- src/data_reader.py
+++ src/data_reader.py
@@ -1,13 +1,35 @@
 import pandas as pd
 import os
 
-print(pd.__version__)
-arr = os.listdir("../data")
 
-for file in arr:
-    values = pd.read_csv("../data/" + file)
-    print(values)
+def clean_filename(filename):
+    if filename[0:4] != "data":
+        filename = "data_" + filename
+    if filename.find(".") != -1:
+        filename = filename[0 : filename.find(".")] + ".csv"
+    else:
+        filename = filename + ".csv"
+    return filename
 
-# values = pd.read_csv("../data/data_Austria.csv")
 
-# print(values)
+def get_filenames():
+    filenames = os.listdir("../data")
+    return filenames
+
+
+def get_country_data(filename):
+    original = filename
+    try:
+        filename = clean_filename(filename)
+        df = pd.read_csv("../data/" + filename)
+        return df
+    except Exception as e:
+        print("Please enter a correct filename, there is no data on " + original)
+
+
+def get_all_data():
+    fnames = get_filenames()
+    df_arr = []
+    for fname in fnames:
+        df_arr.append(get_country_data(fname))
+    return df_arr
