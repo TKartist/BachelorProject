e0c40baf7e557e3965b91f619a61faccf6c767c2
TKartist <rimh@usi.ch>
2024-03-13 15:25:27 +0100

    grid search implemented for finding orders of ARIMA models

---
+++ src/forecasting.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import data_reader as dr
+import itertools
+
+from pmdarima.arima import auto_arima
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.seasonal import seasonal_decompose
+from auxiliary import adf_test
+
+
+def view_trend_seasonality(series):
+    results = seasonal_decompose(series)
+    results.plot()
+    plt.show()
+
+
+# there are obscure cases of auto_arima where the minimum AIC is present after an inverted bell curve
+# hence, in this case, we perform a grid search just in case.
+# This function will be alter the order, if it wasn't intercepted by auto_arima function as it should
+# i.e. (p, d, q) == (0,0,0)
+def grid_search(series, order):
+    if order[0] > 0 or order[2] > 0:
+        # perform grid_search
+        model = ARIMA(series, order=order)
+        return model
+    p = range(1, 7)
+    q = range(1, 7)
+    best_aic = np.inf
+    best_model = None
+    pdq = list(itertools.product(p, order, q))
+    for param in pdq:
+        try:
+            model = ARIMA(series, order=param)
+            results = model.fit()
+            aic = results.aic
+
+            if results.mle_retvals["converged"] and aic < best_aic:
+                best_model = model
+                best_aic = aic
+        except:
+            continue
+    return best_model
+
+
+def arima_order(series, test_size):
+    arima = auto_arima(
+        series,
+        start_p=0,
+        start_q=0,
+        max_p=6,
+        max_q=6,
+        seasonal=False,
+        trace=True,
+        errpr_action="ignore",
+        suppress_warnings=True,
+        stepwise=True,
+    )
+    # train and test set split (assuming we try to predic half a year)
+    train = series[:-test_size]
+    test = series[-test_size:]
+
+    model = grid_search(series, arima.order)
+    results = model.fit()
+
+    start = len(train)
+    end = len(train) + len(test) - 1
+    preds = results.predict(start=start, end=end, typ="levels").rename(
+        "ARIMA predictions"
+    )
+
+    series.plot(legend=True, figsize=(16, 10))
+    preds.plot(legend=True)
+    plt.show()
+
+
+# Seasonal AutoRegressive Integrated Moving Average Model
+def sarima_prediction(series, test_size):
+    sarima = auto_arima(
+        series,
+        start_p=0,
+        start_q=0,
+        max_p=10,
+        max_q=10,
+        seasonal=True,
+        m=12,
+        trace=True,
+    )
+    order = sarima.order
+    seasonal_order = sarima.seasonal_order
+
+    train = series[:-test_size]
+    test = series[-test_size:]
+
+    start = len(train)
+    end = start + len(test)
+
+    model = SARIMAX(
+        train, order=order, seasonal_order=seasonal_order, enforce_invertibility=False
+    )
+    results = model.fit()
+    predictions = results.predict(start, end, typ="levels").rename(
+        "SARIMA PREDICTION " + str(order) + "X" + str(seasonal_order)
+    )
+    series.plot(legend=True, figsize=(16, 10))
+    predictions.plot(legend=True)
+    plt.show()
+
+
+df = dr.organize_table("Germany")
+# view_trend_seasonality(df["demand"])
+arima_order(df["demand"], 12)
+# sarima_prediction(df["demand"], 12)
+# print(df["demand"])
