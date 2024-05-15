1a9cd2cb6027aff6fc9f009eb1ae713e08b06949
TKartist <rimh@usi.ch>
2024-05-15 22:56:10 +0200

    new overall analysis

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import variables as var
 from os import listdir
 from os.path import isfile, join
-from data_reader import organize_table
+from data_reader import organize_table, organize_rebasement
 from statsmodels.tsa.seasonal import seasonal_decompose
 import ast
+from auxiliary import rmspe_calculation
 
 
 def view_trend_seasonality(series):
@@ -50,12 +51,14 @@ def visualize_prediction(prediction, data, title):
     plt.show()
 
 
-def visualize_model_performance(country, energy):
+def visualize_model_performance(country, energy, models):
     series = pd.read_csv(
-        var.result_dir + "prediction_" + country + "_" + energy + "_all.csv",
+        # var.result_dir + "prediction_" + country + "_" + energy + "_all.csv",
+        "../demand_forecasts/prediction_" + country + "_" + energy + "_all.csv",
         index_col=var.DATE,
     )
-    series.boxplot(column=[var.SARIMA, var.ARIMA, var.DL, var.SARIMAX])
+    # series.boxplot(column=[var.SARIMA, var.ARIMA, var.DL, var.SARIMAX])
+    series.boxplot(column=models)
     plt.xlabel("model")
     plt.ylabel(var.RMSPE)
     plt.show()
@@ -152,11 +155,71 @@ def visualize_pred_margin(country, energy):
     plt.show()
 
 
-def visual_narrative(c, e):
-    df = organize_table(c)[e]
-    view_trend_seasonality(df)
-    visualize_pred_margin(c, e)
-    visualize_model_performance(c, e)
+# def visual_narrative(c, e):
+#     df = organize_table(c)[e]
+#     view_trend_seasonality(df)
+#     visualize_pred_margin(c, e)
+#     visualize_model_performance(c, e)
 
 
-visual_narrative("France", "solar")
+# visual_narrative("France", "solar")
+
+
+def iterative_forecast_visualization(energy, country, models):
+    data = organize_table(country)[energy]
+    exogenous = organize_rebasement(country, energy, max(data.index))
+    path = "../demand_forecasts/"
+    df = pd.read_csv(
+        path + "graph_" + country + "_" + energy + ".csv",
+        index_col="date",
+        parse_dates=True,
+    )
+    for model in models:
+        df[model] = df[model].apply(lambda x: ast.literal_eval(x))
+        rmspes = []
+        for iter in range(3):
+            s = df.index[iter]
+            e = s + pd.DateOffset(months=len(df.index) - 1)
+            dates = pd.date_range(start=s, end=e, freq="MS")
+            z = pd.DataFrame(dates, columns=["date"])
+            y = []
+            for a in df[model]:
+                y.append(a[iter])
+            z["predictions"] = y
+            z = z.set_index("date")
+            z["demand"] = data[data.index[data.index >= min(z.index)]]
+            rmspes.append(rmspe_calculation(z["demand"], z["predictions"]))
+            z.index.freq = "MS"
+            plt.plot(
+                dates, y, label="M+" + str(iter + 1) + " forecasts", linestyle="--"
+            )
+        data[84:].rename("monthly data").plot(legend=True)
+        exogenous["rebasement"][84:].rename("hourly data cumulation").plot(legend=True)
+        # df["median"].rename("sarimax model forecast").plot(legend=True)
+        plt.legend()
+        plt.title(model + " model forecast of " + energy + " in " + country)
+        plt.gcf().set_size_inches(10, 6)
+        plt.savefig(
+            path + model + " model forecast of " + energy + " in " + country + ".png"
+        )
+        plt.show()
+        plt.title(model + " M+alpha RMSPE of " + energy + " in " + country)
+        plt.bar(
+            ["M+1 RMSPE", "M+2 RMSPE", "M+3 RMSPE"],
+            rmspes,
+            width=0.3,
+        )
+        plt.savefig(
+            path + model + " M+alpha RMSPE of " + energy + " in " + country + ".png"
+        )
+        plt.show()
+    title = ""
+    for model in models:
+        title = title + model + ", "
+    title = title + "performance of " + energy + " forecast in " + country
+    plt.title(title)
+    plt.savefig(path + title + ".png")
+    visualize_model_performance(country, energy, models)
+
+
+iterative_forecast_visualization("demand", "Romania", ["SARIMAX", "SARIMA", "DL"])
